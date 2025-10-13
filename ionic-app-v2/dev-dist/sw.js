d2fb6149737de011949a2235aee0ea930c84ccdd
ambyltd <ambyltd@gmail.com>
2025-10-13 21:28:58 +0000

    update: mobile app submodule with dev environment fixes

--- ionic-app-v2/dev-dist/sw.js
+++ ionic-app-v2/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-4c29871c'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.dq56bl5hpgg"
+    "revision": "0.07iu95eqe4g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
